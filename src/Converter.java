7e37f62f5affea32545654d2ca412162a0e58d4d
Ckerius <ckerius@yandex.ru>
2022-10-19 13:30:01 +0500

    Программа написана и работает как заявлено в ТЗ.

--- src/Converter.java
+++ src/Converter.java
@@ -1,16 +1,8 @@
 public class Converter {
     public static int calories(int steps) {
-        int kcal = steps * 5 / 100;
-        return kcal;
+        return steps * 5 / 100;
     }
     public static double distance(int steps) {
-        double km = steps * 0.75 / 1000;
-        return km;
+        return steps * 0.75 / 1000;
     }
 }
-
-/*
-В этом классе осуществляется преобразование шагов в километры и калории.
-    Для подсчёта дистанции можно считать, что один шаг равен 75 см.
-    Для подсчёта количества сожжённых килокалорий можно считать, что 1 шаг = 50 калорий, 1 килокалория = 1 000 калорий.
- */
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,27 +1,123 @@
 public class StepTracker {
 
-}
-/*
-Это класс с основной логикой приложения — в нём хранится информация о пройденных шагах,
- реализована логика по сохранению и изменению количества шагов, а так же рассчитывается статистика.
-В памяти приложения хранится следующая информация:
-    Данные о шагах, пройденных в каждый день месяца. В году 12 месяцев и для удобства считаем, что в каждом ровно 30 дней.
-    Целевое количество шагов. При старте приложения устанавливается равным 10 000 в день.
 
-    В классе должна быть реализована следующая функциональность:
-1. Сохранение количества шагов за день. Количество шагов должно быть неотрицательным.
-   Пользователь должен указать номер месяца (начиная с 0), номер дня и количество шагов, пройденных в этот день.
-   Для ускорения прототипирования на данном этапе считается, что в месяце ровно 30 дней.
-   Если за какой-то день статистика не сохранялась, то считаем количество шагов в этот день равным 0.
-2.Подсчёт и вывод статистики за указанный пользователем месяц. В статистике должны быть следующие данные:
-    Количество пройденных шагов по дням в следующем формате:
-    1 день: 3000, 2 день: 2000, ..., 30 день: 10000
-    Общее количество шагов за месяц;
-    Максимальное пройденное количество шагов в месяце;
-    Среднее количество шагов;
-    Пройденная дистанция (в км);
-    Количество сожжённых килокалорий;
-    Лучшая серия: максимальное количество подряд идущих дней,
-        в течение которых количество шагов за день было равно или выше целевого.
-3.Изменение целевого количества шагов. Данные вводятся пользователем. Введённое значение не должно быть отрицательным.
- */
+    static int target = 10000;
+     MonthData[] monthToData;
+
+    public StepTracker() {
+        monthToData = new MonthData[12];
+        for (int i = 0; i < monthToData.length; i++) {
+            monthToData[i] = new MonthData();
+        }
+    }
+    public static int returnTarget() {
+        return target;
+    }
+    public void inputSteps(int month, int day, int steps) {
+        MonthData monthToDataId = monthToData[month];
+        monthToDataId.setSteps(day, steps);
+    }
+    public void outputSteps(int month) {
+        MonthData monthToDataId = monthToData[month];
+        monthToDataId.getSteps();
+    }
+    public void getAllSteps(int month) {
+        MonthData monthToDataId = monthToData[month];
+        monthToDataId.getAllSteps();
+    }
+    public void getDayWithMaxSteps(int month) {
+        MonthData monthToDataId = monthToData[month];
+        monthToDataId.getMax();
+    }
+    public  void getTheMeanSteps(int month) {
+        MonthData monthToDataId = monthToData[month];
+        monthToDataId.getTheMean();
+    }
+    public void calcCalories(int month) {
+        MonthData monthToDataId = monthToData[month];
+        monthToDataId.calories();
+    }
+    public void calcDistance(int month) {
+        MonthData monthToDataId = monthToData[month];
+        monthToDataId.distance();
+    }
+    public  void getBestSeries(int month) {
+        MonthData monthToDataId = monthToData[month];
+        monthToDataId.series();
+    }
+    public void changeTarget(int newTarget) {
+        if (newTarget >= 0) {
+            target = newTarget;
+        } else {
+            System.out.println("Ошибка: отрицательное число");
+        }
+    }
+
+}
+class MonthData {
+    int[] daySteps = new int[30];
+
+    void setSteps(int day, int steps) {
+        daySteps[day] = steps;
+    }
+     public void getSteps() {
+        for (int i = 0; i < daySteps.length; i++) {
+           System.out.print((i + 1) + " день: " + daySteps[i] + ", ");
+        } System.out.println();
+
+    }
+    public void getAllSteps() {
+        int result = 0;
+        for (int i: daySteps) {
+            result = result + i;
+        }
+        System.out.println("Всего за месяц было пройдено " + result + " шагов.");
+    }
+    public void getMax() {
+        int max = 0;
+        for (int i = 0; i < daySteps.length; i++) {
+            if (max < daySteps[i]){
+                max = daySteps[i];
+            }
+        }
+        System.out.println("Максимальное пройденное количество шагов - " + max + ".");
+    }
+    public void getTheMean() {
+        int sum = 0;
+        for (int i = 0; i < daySteps.length; i++) {
+            sum = sum + daySteps[i];
+        }
+        int result = sum / daySteps.length;
+        System.out.println("В среднем за месяц вы прошли " + result + " шагов в день.");
+    }
+    public void calories() {
+        int stepsSum = 0;
+        for (int i = 0; i < daySteps.length; i++) {
+            stepsSum = stepsSum + daySteps[i];
+        }
+        System.out.println("Всего потрачено " + Converter.calories(stepsSum) + " ккал.");
+    }
+    public void distance() {
+        int stepsSum = 0;
+        for (int i = 0; i < daySteps.length; i++) {
+            stepsSum = stepsSum + daySteps[i];
+        }
+        System.out.println("Всего пройдено " + Converter.distance(stepsSum) + " км.");
+    }
+    public void series() {
+        int currentSeries = 0;
+        int bestSeries = 0;
+        int target = StepTracker.returnTarget();
+        for (int i = 0; i < daySteps.length; i++) {
+            if (daySteps[i] >= target) {
+                currentSeries = currentSeries + 1;
+            } else {
+                if (bestSeries <= currentSeries) {
+                    bestSeries = currentSeries;
+                }
+                currentSeries = 0;
+            }
+        }
+        System.out.println("Лучшая серия за месяц - " + bestSeries + " дней в цели.");
+    }
+}
